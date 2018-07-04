5ea6d145a3fd1fc10a1a3df2e24572689751eae6
kastrup1 <jkastrupb@gmail.com>
2018-07-04 00:28:18 -0300

    add tutorial api https://scotch.io/tutorials/build-a-rest-api-with-django-a-test-driven-approach-part-1

---
+++ apituto/djangorest/api/urls.py
@@ -0,0 +1,19 @@
+# api/urls.py
+
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import CreateView
+from .views import DetailsView 
+
+
+
+urlpatterns = {
+	url(r'^auth/', include('rest_framework.urls', # ADD THIS URL
+                               namespace='rest_framework')), 
+    url(r'^bucketlists/$', 
+        CreateView.as_view(), name="create"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$',
+        DetailsView.as_view(), name="details"),
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
