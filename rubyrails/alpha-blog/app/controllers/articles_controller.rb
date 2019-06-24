8bd2d4e613d5fa2e9865ac289c9063a47aa25a76
Júlia <jkastrupb@gmail.com>
2019-06-24 18:19:01 -0300

    kadu fixedit

---
+++ rubyrails/alpha-blog/app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to articles_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @article = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description )
+  end
+
+
+end
